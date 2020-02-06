9bdad72bad6ceb73edc69922198b7c1cdd98d9bd
unknown <adebayoadesanya710@gmail.com>
2020-02-06 13:14:53 +0100

    updated to accept more than one client

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from time import ctime
-def get_int_input(prompt):
+
+def save_data(client_name, data):
+    DATA.append(
+        {
+            'Customer_name': client_name, 
+            'product': data,
+            'time':ctime()
+        }
+    )
+
+    DATA.append(data)
+
+def end_program(data=None):
+    if data is not None:
+        save_data(client_name, data)
+
+    print(DATA)
+    exit()
+
+def get_number_input(prompt):
+
+
     value = None
+
+
+
     while type(value) != float:
         try:
             value = float(input(prompt))
@@ -11,33 +35,66 @@ def get_int_input(prompt):
 
         except ValueError as e:
             print("inValid input")
-            print(e, File=ERROR_FILE)
-            print(ctime(), e, file = ERROR_FILE)
+            print(ctime(), e, file=ERROR_FILE)
+
+
 
 def main():
     while True:
         client_name = input('what is the customer\'s name:')
+    
+
         if not client_name:
-            break
+            print('Name must be provided')
+            continue
+
+        elif client_name == '-1':
+            end_program();
+
+        product_information = []
 
         while True:
             product_name = input('what is the product name?:')
+
             if not product_name:
+                print('product name must be provided!')
+                continue
+
+            elif product_name == '-1':
+                #end_program(client_name, data=product_information)
+                save_data(client_name,product_information)
+                break
+            
+            product_qty = get_number_input(f'How many {product_name} purchase?:')
+            
+            if product_qty == -1:
+                #end_program(client_name, data=product_information)
+                save_data(client_name,product_information)
+                break
+        
+            product_price = get_number_input(f"How much is {product_name}?:")
+
+            if product_price == -1:
+                #end_program(client_name, data=product_information)
+                save_data(client_name,product_information)
                 break
 
-            
-            product_qty = get_int_input(f'How many {product_name} purchase?:')
-        
-            producr_price = input(f"How much is {product_name}?:")
+            product_information.append(
+                {
+                    'product_name' :product_name,
+                    'product_quantity' :product_qty,
+                    'product_price' :product_price
 
-    
+                }
+            )    
+
 
 if __name__ == '__main__':
 
     #SUPREGOBALS
 
     ERROR_FILE = open('error_log.text', 'a')
-
+    DATA = []
 
     main()
 
